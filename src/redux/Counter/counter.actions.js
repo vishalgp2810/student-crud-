d3b57bb41790487169bd081cfb8fcbbd2e8375aa
sociomee_social <sociomee.social@gmail.com>
2021-10-21 19:34:48 +0530

    first commit

--- src/redux/Counter/counter.actions.js
+++ src/redux/Counter/counter.actions.js
@@ -2,21 +2,13 @@ import { INCREMENT, DECREMENT } from './counter.types';
 
 
     export const increaseCounter = () => {
-
         return {
-
             type: INCREMENT,
-
         };
-
     };
 
     export const decreaseCounter = () => {
-
         return {
-
            type: DECREMENT,
-
         };
-
     };
